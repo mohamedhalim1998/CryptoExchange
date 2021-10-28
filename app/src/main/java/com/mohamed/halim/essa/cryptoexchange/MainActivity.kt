6e4de09ead74c5432839912024769bfb1bd4ff5c
MohamedHalim <mohamedhalim1998@gmail.com>
2021-10-28 20:26:26 +0200

    initial commit

---
+++ app/src/main/java/com/mohamed/halim/essa/cryptoexchange/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.mohamed.halim.essa.cryptoexchange
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.mohamed.halim.essa.cryptoexchange.ui.theme.CryptoExchangeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptoExchangeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CryptoExchangeTheme {
+        Greeting("Android")
+    }
+}
