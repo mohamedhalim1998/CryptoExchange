8c63c6f435f10668b5cc93dd5f44a80a24021f78
MohamedHalim <mohamedhalim1998@gmail.com>
2021-10-30 21:32:48 +0200

    ADD: implement get currency info request

--- app/src/main/java/com/mohamed/halim/essa/cryptoexchange/data/network/ApiService.kt
+++ app/src/main/java/com/mohamed/halim/essa/cryptoexchange/data/network/ApiService.kt
@@ -1,11 +1,14 @@
 package com.mohamed.halim.essa.cryptoexchange.data.network
 
 
+import com.mohamed.halim.essa.cryptoexchange.data.network.model.CurrencyInfo
 import com.mohamed.halim.essa.cryptoexchange.data.network.model.RateExchangeResponse
 import com.mohamed.halim.essa.cryptoexchange.utils.API_KEY
 import retrofit2.http.GET
 
 interface ApiService {
-    @GET("/v1/exchangerate/USD?filter_asset_id=[,SHIB,DOGE,DOT,XRP,SOL,ADA,ADA,USDT,BNB,ETH,BTC,]&invert=true&$API_KEY")
+    @GET("/v1/exchangerate/USD?filter_asset_id=[,SHIB,DOGE,DOT,XRP,SOL,ADA,USDT,BNB,ETH,BTC,]&invert=true&$API_KEY")
     suspend fun getRates(): RateExchangeResponse
+    @GET("/v1/assets?filter_asset_id=[,SHIB,DOGE,DOT,XRP,ADA,USDT,BNB,ETH,BTC,SOL,]&apikey=40EBD296-8592-4B3E-9DAF-8A6E398C1D92")
+    suspend fun getCurrencyInfo(): List<CurrencyInfo>
 }
---
+++ app/src/main/java/com/mohamed/halim/essa/cryptoexchange/data/network/model/CurrencyInfo.kt
@@ -0,0 +1,13 @@
+package com.mohamed.halim.essa.cryptoexchange.data.network.model
+
+import com.google.gson.annotations.SerializedName
+
+
+data class CurrencyInfo(
+    @SerializedName("asset_id")
+    val currencyId: String,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("type_is_crypto")
+    val isCrypto: Boolean,
+)
