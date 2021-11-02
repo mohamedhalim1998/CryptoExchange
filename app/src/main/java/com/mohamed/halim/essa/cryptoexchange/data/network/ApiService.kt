caabd8240ca569dc49c4f18f1a45087cf7f47640
MohamedHalim <mohamedhalim1998@gmail.com>
2021-11-02 18:31:21 +0200

    EDIT: fix api end point

--- app/src/main/java/com/mohamed/halim/essa/cryptoexchange/data/network/ApiService.kt
+++ app/src/main/java/com/mohamed/halim/essa/cryptoexchange/data/network/ApiService.kt
@@ -7,8 +7,8 @@ import com.mohamed.halim.essa.cryptoexchange.utils.API_KEY
 import retrofit2.http.GET
 
 interface ApiService {
-    @GET("/v1/exchangerate/USD?filter_asset_id=[,SHIB,DOGE,DOT,XRP,SOL,ADA,USDT,BNB,ETH,BTC,]&invert=true&$API_KEY")
+    @GET("v1/exchangerate/USD?filter_asset_id=[,SHIB,DOGE,DOT,XRP,SOL,ADA,USDT,BNB,ETH,BTC,]&invert=true&apikey=$API_KEY")
     suspend fun getRates(): RateExchangeResponse
-    @GET("/v1/assets?filter_asset_id=[,SHIB,DOGE,DOT,XRP,ADA,USDT,BNB,ETH,BTC,SOL,]&apikey=40EBD296-8592-4B3E-9DAF-8A6E398C1D92")
+    @GET("v1/assets?filter_asset_id=[,SHIB,DOGE,DOT,XRP,ADA,USDT,BNB,ETH,BTC,SOL,]&apikey=$API_KEY")
     suspend fun getCurrencyInfo(): List<CurrencyInfo>
 }
