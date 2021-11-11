bea7f3a8efbf490470cb15b166810d304c519b2b
MohamedHalim <mohamedhalim1998@gmail.com>
2021-11-11 21:02:16 +0200

    ADD: implement basic database dao

---
+++ app/src/main/java/com/mohamed/halim/essa/cryptoexchange/data/local/CryptoDao.kt
@@ -0,0 +1,42 @@
+package com.mohamed.halim.essa.cryptoexchange.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.mohamed.halim.essa.cryptoexchange.data.local.model.CryptoCurrencyLocal
+import com.mohamed.halim.essa.cryptoexchange.data.local.model.RateHistoryLocal
+import com.mohamed.halim.essa.cryptoexchange.utils.HistoryPeriod
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface CryptoDao {
+    @Query("SELECT * FROM crypto WHERE `from` = :from")
+    fun getCurrentRates(from: String): Flow<List<CryptoCurrencyLocal>>
+
+    @Query("SELECT * FROM `Rate History` WHERE `from` = :from AND historyPeriod = :historyPeriod")
+    fun getCryptoHistoryHour(
+        from: String,
+        historyPeriod: HistoryPeriod
+    ): Flow<List<RateHistoryLocal>>
+
+    @Insert
+    suspend fun insertCrypto(cryptoCurrencyLocal: CryptoCurrencyLocal)
+
+    @Insert
+    suspend fun insertCrypto(cryptoCurrencyLocalList: List<CryptoCurrencyLocal>)
+
+    @Insert
+    suspend fun insertRateHistory(rateHistoryLocal: RateHistoryLocal)
+
+    @Insert
+    suspend fun insertRateHistory(rateHistoryLocalList: List<RateHistoryLocal>)
+
+
+    @Query("DELETE FROM crypto")
+    suspend fun deleteCrypto()
+
+    @Query("DELETE FROM `Rate History` WHERE historyPeriod = :historyPeriod")
+    suspend fun deleteHistoryRate(historyPeriod: HistoryPeriod)
+
+}
