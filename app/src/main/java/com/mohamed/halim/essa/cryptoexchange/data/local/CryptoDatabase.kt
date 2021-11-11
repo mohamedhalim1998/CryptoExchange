306bce33b1b4f6e269ae8c202aac10afd5e93a58
MohamedHalim <mohamedhalim1998@gmail.com>
2021-11-11 20:58:02 +0200

    ADD: basic room database impl

---
+++ app/src/main/java/com/mohamed/halim/essa/cryptoexchange/data/local/CryptoDatabase.kt
@@ -0,0 +1,12 @@
+package com.mohamed.halim.essa.cryptoexchange.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.mohamed.halim.essa.cryptoexchange.data.local.model.CryptoCurrencyLocal
+import com.mohamed.halim.essa.cryptoexchange.data.local.model.RateHistoryLocal
+import com.mohamed.halim.essa.cryptoexchange.utils.RoomConverters
+
+@Database(entities = [CryptoCurrencyLocal::class, RateHistoryLocal::class], version = 1)
+@TypeConverters(RoomConverters::class)
+abstract class CryptoDatabase : RoomDatabase()
---
+++ app/src/main/java/com/mohamed/halim/essa/cryptoexchange/utils/RoomConverters.kt
@@ -0,0 +1,16 @@
+package com.mohamed.halim.essa.cryptoexchange.utils
+
+import androidx.room.TypeConverter
+
+class RoomConverters {
+    @TypeConverter
+    fun convertFromHistoryPeriod(historyPeriod: HistoryPeriod): String {
+        return historyPeriod.value
+    }
+
+    @TypeConverter
+    fun convertToHistoryPeriod(historyPeriod: String): HistoryPeriod {
+        return HistoryPeriod.valueOf(historyPeriod)
+    }
+
+}
