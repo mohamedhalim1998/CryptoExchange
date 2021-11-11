da01da5535bee68a9a1c6e5b513bb888f35682a9
MohamedHalim <mohamedhalim1998@gmail.com>
2021-11-11 20:54:54 +0200

    ADD: local data model with room db

---
+++ app/src/main/java/com/mohamed/halim/essa/cryptoexchange/data/local/model/CryptoCurrencyLocal.kt
@@ -0,0 +1,15 @@
+package com.mohamed.halim.essa.cryptoexchange.data.local.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "crypto")
+data class CryptoCurrencyLocal(
+    @PrimaryKey
+    val cryptoId: String,
+    val from: String,
+    val name: String,
+    val icon: String,
+    val rate: Double,
+    val time: Long,
+)
---
+++ app/src/main/java/com/mohamed/halim/essa/cryptoexchange/data/local/model/RateHistoryLocal.kt
@@ -0,0 +1,22 @@
+package com.mohamed.halim.essa.cryptoexchange.data.local.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.mohamed.halim.essa.cryptoexchange.utils.HistoryPeriod
+
+
+@Entity(tableName = "Rate History")
+data class RateHistoryLocal(
+    val from: String,
+    val historyPeriod: HistoryPeriod,
+    val rateClose: Double,
+    val rateHigh: Double,
+    val rateLow: Double,
+    val rateOpen: Double,
+    val timeClose: Long,
+    val timeOpen: Long,
+    val timePeriodEnd: Long,
+    val timePeriodStart: Long,
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = -1,
+)
